9d9446a8780de1cbc08e863d5af44da3a51df671
udayred <udayred@LT-A0062317.corp.cdw.com>
2018-06-30 10:55:16 -0500

    updated project with tomcat scripts, appsec.yml

--- src/main/java/com/msp/SampleApplication.java
+++ src/main/java/com/msp/SampleApplication.java
@@ -1,15 +1,20 @@
 package com.msp;
- 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
- 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 @SpringBootApplication
-public class SampleApplication {
-     
-     
- 
-        public static void main(String[] args) {
-            SpringApplication.run(SampleApplication.class, args);
-        }
- 
+public class SampleApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SampleApplication.class);
+	}
+
 }
