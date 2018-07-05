995245aa817c2c5822482e54a9a4ddd343ddcad2
udayred <udayred@LT-A0062317.corp.cdw.com>
2018-07-05 09:19:41 -0500

    test commit - updated the text format

--- src/main/java/com/msp/controller/MspController.java
+++ src/main/java/com/msp/controller/MspController.java
@@ -8,7 +8,7 @@ public class MspController {
      
     @RequestMapping("/msp")
     public String sayHello() {
-        return "<h2>This is MSP- java - springboot application</h2>";
+        return "This is MSP- java - springboot application";
     }
  
 }
