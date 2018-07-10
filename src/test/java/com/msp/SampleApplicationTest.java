9ba1283e339fd93cac4a66300f8c2c587faa3766
udayred <udayred@LT-A0062317.corp.cdw.com>
2018-07-09 21:29:06 -0500

    updated junit to 4.12 and added sampleapplicationtest class

---
+++ src/test/java/com/msp/SampleApplicationTest.java
@@ -0,0 +1,25 @@
+package com.msp;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SampleApplicationTest {
+	@Before
+	public void setup() {
+		System.out.println("setup method");
+	}
+
+	@Test
+	public void thatApplicationTest() {
+		System.out.println("thatApplicationTest method");
+	}
+	@Test
+	public void thatApplicationTest1() {
+		System.out.println("thatApplicationTest1 method");
+	}
+	@After
+	public void teardown() {
+		System.out.println("teardown method");
+	}
+}
